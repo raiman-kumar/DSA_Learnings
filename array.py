22f82e9103fc905c52bcdbe04e209598342351ea
raiman kumar <raiman.rks@gmail.com>
2026-08-03 13:21:42 +0530

    second largest in list

--- array.py
+++ array.py
@@ -21,9 +21,37 @@
 #     count += 1
 # print(count)
 
-n = 5
-count = 0
-for i in range(n):
-    for j in range(n):
-        count += 1
-print(count)
+# n = 5
+# count = 0
+# for i in range(n):
+#     for j in range(n):
+#         count += 1
+# print(count)
+
+# n = 1000
+# count = 0
+
+# while n > 0:
+#     count += 1
+#     n = n // 2
+
+# print(count)
+
+# mylist = [10, 5, 18, 7, 20, 3]
+# largest = mylist[0]
+# second_largest = mylist[0]
+# for i in mylist:
+#     if i > largest:
+#         largest = i
+# print(largest)
+
+mylist = [10, 5, 18, 7, 20, 3]
+largest = mylist[0]
+second_largest = mylist[0]
+for i in mylist:
+    if i > largest:
+        second_largest = largest
+        largest = i
+    elif i > second_largest and i != largest:
+        second_largest = i
+print(second_largest)
