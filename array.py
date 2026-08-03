5e700663990879e5163bd5c4654a31c2d8814aad
raiman kumar <raiman.rks@gmail.com>
2026-08-03 11:46:13 +0530

    array.py done

---
+++ array.py
@@ -0,0 +1,29 @@
+# # array in DSA
+# # array applies through list in python
+
+# myarr = [12,18,16,14,22,11]
+# print(myarr)
+
+# n = 5000
+# print("start")
+# for i in range(n):
+#     print(i)
+# print("stop")
+
+# n = 5
+# for i in range(n):
+#     for j in range(n):
+#         print(i,j)
+
+# n = 5
+# count = 0
+# for i in range(n):
+#     count += 1
+# print(count)
+
+n = 5
+count = 0
+for i in range(n):
+    for j in range(n):
+        count += 1
+print(count)
