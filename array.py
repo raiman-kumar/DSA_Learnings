3073c022202f0cf11b9907d2d96419dfb72f6b6f
raiman kumar <raiman.rks@gmail.com>
2026-08-13 20:34:48 +0530

    linked list class created

--- array.py
+++ array.py
@@ -316,23 +316,28 @@
 # counting sort
 
 
-def counting_sort(array):
-    if not array:
-        return array
+# def counting_sort(array):
+#     if not array:
+#         return array
         
-    max_val = max(array)
-    count = [0] * (max_val + 1)
+#     max_val = max(array)
+#     count = [0] * (max_val + 1)
 
-    for num in array:
-        count[num] += 1
+#     for num in array:
+#         count[num] += 1
         
-    array[:] = []
+#     array[:] = []
 
-    for num, freq in enumerate(count):
-        array.extend([num] * freq)
+#     for num, freq in enumerate(count):
+#         array.extend([num] * freq)
 
-    return array
+#     return array
 
-unsorted_arr = [4, 7, 3, 2, 2, 6, 3, 3, 1, 6, 5, 2, 3]
-sorted_arr = counting_sort(unsorted_arr)
-print("Sorted array:", sorted_arr)
+# unsorted_arr = [4, 7, 3, 2, 2, 6, 3, 3, 1, 6, 5, 2, 3]
+# sorted_arr = counting_sort(unsorted_arr)
+# print("Sorted array:", sorted_arr)
+
+
+class linked_list:
+    data = None
+    address = None
