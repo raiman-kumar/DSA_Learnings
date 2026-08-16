33659598960eb929ce38780e5a1c3e48c43fe3f1
raiman kumar <raiman.rks@gmail.com>
2026-08-16 16:44:47 +0530

    post and delete method

--- session01.py
+++ session01.py
@@ -3,12 +3,21 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
+# call at 127.0.0.1:8000
+@app.get('/') # from browser only get method will call 
 def show():
     return 'hello world'
 
-@app.post('/user/{username}/')
-def user(username):
-    name = username
+# for other route except get you need Swagger UI or API testing tool like Postman
+# call at 127.0.0.1:8000/docs
+
+@app.post('/user/{firstname}/') # path parameter
+def user(firstname,lastname): # query parameter
+    name = firstname + lastname
     return f'user name is {name}'
 
+names = ['aman','rohit','sahil']
+@app.delete('/user')
+def user(name):
+    names.remove(name)
+    return names
